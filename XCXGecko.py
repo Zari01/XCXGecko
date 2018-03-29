0c52adab69c71b0506955e6b647efb33acd4d619
Zari01 <max@ramblemuse.com>
2018-03-28 20:38:04 -0700

    Update XCXGecko.py

--- XCXGecko.py
+++ XCXGecko.py
@@ -201,9 +201,10 @@ class XCXGeckoMainWindow(GeckoMainWindow):
   def populateCodeOffsets(self):
     self.cmb_code_offset.addItems(['0 (default)',
                                    '-20480 (-0x5000: 5.3.2 LoadiineV4+pyGecko)',
+                                   '8192 (+0x2000: Haxchi+TCPGecko 5.5.2 US?)'
                                    '45056 (+0xB000: Loadiine GX2 0.3 / 5.5.X US)',
                                    '53248 (+0xD000: 5.5.X EU/JP)',
-                                   '61440 (+0xF000: Haxchi+TCPGecko 5.5.2 US)'])
+                                   '61440 (+0xF000: Haxchi+TCPGecko 5.5.2 US?)'])
 
   @pyqtSlot()
   def onHomeURL(self):
